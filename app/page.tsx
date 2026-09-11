6b79b06fc928b89820b5401d8a222ae3deb80e12
Anaïs Aerts <a.waerks@gmail.com>
2026-09-11 17:01:19 +0200

    vercel analytics

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import AppShell from "@/components/AppShell";
 import PWARegister from "@/components/PWARegister";
 import { schedule } from "@/lib/schedule";
-export default function Page(){return <><PWARegister/><AppShell schedule={schedule}/></>;}
+import { Analytics } from '@vercel/analytics/next';
+export default function Page(){return <><PWARegister/><AppShell schedule={schedule}/><Analytics /></>;}
